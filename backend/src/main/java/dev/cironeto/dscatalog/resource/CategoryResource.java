8cae4507579d65844f61efb99795047cba4102ed
Ciro Neto <ciro.neto16@gmail.com>
2021-11-11 21:09:48 -0300

    set H2 database

---
+++ backend/src/main/java/dev/cironeto/dscatalog/resource/CategoryResource.java
@@ -0,0 +1,27 @@
+package dev.cironeto.dscatalog.resource;
+
+import dev.cironeto.dscatalog.entity.Category;
+import dev.cironeto.dscatalog.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = categoryService.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+}
---
+++ backend/src/main/java/dev/cironeto/dscatalog/service/CategoryService.java
@@ -0,0 +1,19 @@
+package dev.cironeto.dscatalog.service;
+
+import dev.cironeto.dscatalog.entity.Category;
+import dev.cironeto.dscatalog.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll(){
+        return categoryRepository.findAll();
+    }
+}
