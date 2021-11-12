c07d171047467ef66779426da4e6d99694618bb8
Ciro Neto <ciro.neto16@gmail.com>
2021-11-11 22:05:55 -0300

    create database seed - data.sql

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_category (name) VALUES
+            'Books',
+            'test',
+            'Electronics',
+            'Computers',
+            'Smartphones',
+            'Tablets';
