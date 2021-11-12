48d8ae091d5519e21cb53b0959024dc826ea151f
Ciro Neto <ciro.neto16@gmail.com>
2021-11-11 22:44:50 -0300

    create DTO class and set the methods

--- backend/src/main/java/dev/cironeto/dscatalog/entity/Category.java
+++ backend/src/main/java/dev/cironeto/dscatalog/entity/Category.java
@@ -10,22 +10,22 @@ public class Category implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
 
     public Category(){
     }
 
-    public Category(long id, String name) {
+    public Category(Long id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- backend/src/main/java/dev/cironeto/dscatalog/resource/CategoryResource.java
+++ backend/src/main/java/dev/cironeto/dscatalog/resource/CategoryResource.java
@@ -1,6 +1,6 @@
 package dev.cironeto.dscatalog.resource;
 
-import dev.cironeto.dscatalog.entity.Category;
+import dev.cironeto.dscatalog.dto.CategoryDTO;
 import dev.cironeto.dscatalog.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,8 +18,8 @@ public class CategoryResource {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = categoryService.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+        List<CategoryDTO> list = categoryService.findAll();
         return ResponseEntity.ok(list);
     }
 
